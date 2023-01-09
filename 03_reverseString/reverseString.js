d9e0e8ab1f2d066d6e229b56906291b973c00ec0
Sofs20 <barbarasofia20@gmail.com>
2023-01-09 20:10:42 +0000

    completed exercises 1, 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+/*
+Strings in JavaScript cannot be reversed directly so you're
+going to have to split it into something else first.. do 
+the reversal and then join it back together into a string.*/
 
-};
 
-// Do not edit below this line
+function reverseString(str){
+    const myArray = str.split("");
+    let reverseArray = myArray.reverse();
+    let joinArray = reverseArray.join(""); 
+    return joinArray;  
+}
+
+let result = reverseString("hello there");
+console.log(result);
+   
 module.exports = reverseString;
