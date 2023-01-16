3b70c9386a3c129856bc9bf196f4e0473a2b93da
Sofs20 <barbarasofia20@gmail.com>
2023-01-16 23:30:07 +0000

    completed version

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,40 @@
-const sumAll = function() {
+/*Implement a function that takes 2 integers and returns 
+the sum of every number between(and including) them:
+
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+
+Hints
+Think about how you would do this on pen and paper and 
+then how you might translate that process into code:
+
+make sure you pay attention to the function parameters
+create a variable to hold the final sum
+loop through the given numbers (link)
+on each iteration add the number to the sum
+return the sum after finishing the loop*/
+
+
+const sumAll = function sumTotal(x, y) {
+
+    if ((typeof x !== 'number') || (typeof y !== 'number')){
+        return "ERROR";
+    }
+
+    if((x < 0) || (y<0)){
+        return "ERROR";
+    }
+     
+    if (x>y){
+        let temp = y;
+        y = x;
+        x = temp;
+    }
+
+    let sum = 0;
+    for (let i = x; i < y + 1; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
